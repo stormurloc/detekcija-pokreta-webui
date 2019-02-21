969752e66632fa1693e92c45dfd0e97b47b36aab
Rastko Stanickov <stanickov.rastko@jjzmaj.edu.rs>
2019-02-21 16:33:24 +0100

    Prosireno prikazivanje promene podataka

--- script.js
+++ script.js
@@ -7,6 +7,20 @@ var app = new Vue({
 	}
 })
 
+var i = 0;
 socket.on('update', function(data){
-	app.message = data
+	console.log(data)
+	if(data == 123 && i < 5){
+		app.message = app.message + '+'
+		i++
+		console.log(i)
+	}
+	else if(data == 321 && i > 0){
+		i--
+		app.message = ''
+		for(j = 0; j < i; j++){
+			app.message = app.message + '+'
+		}
+		console.log(i)
+	}
 })
